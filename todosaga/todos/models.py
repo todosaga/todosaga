b82128ae5389aec64a90b989c3bd027eeda6e31d
조승준 <joseungjun@MacBookPro.office10.ogqcorp.com>
2025-04-04 15:04:17 +0900

    add swagger

--- todosaga/todos/models.py
+++ todosaga/todos/models.py
@@ -1,22 +1,28 @@
 from django.db import models
-
+from django.utils import timezone
 
 class Todo(models.Model):
+    # Todo types
+    CHECK = "check"
+    TIMER = "timer"
     TODO_TYPE_CHOICES = [
-        ("check", "Check"),
-        ("Timer", "Timer"),
+        (CHECK, "Check"),
+        (TIMER, "Timer"),
     ]
-
+    
     title = models.CharField(max_length=255)
+    # Storing categories as a JSON list; PostgreSQL’s JSONField is used here
     categories = models.JSONField()
     type = models.CharField(max_length=10, choices=TODO_TYPE_CHOICES)
+    # For timer-type todos, store the duration in seconds
     duration_seconds = models.IntegerField(null=True, blank=True)
     completed = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
-
+    
+    # Timer-specific fields
     start_time = models.DateTimeField(null=True, blank=True)
     completed_at = models.DateTimeField(null=True, blank=True)
     gained_exp = models.IntegerField(default=0)
-
+    
     def __str__(self):
-        return str(self.title)
+        return self.title
--- todosaga/todosaga/urls.py
+++ todosaga/todosaga/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.contrib.auth.views import LoginView
 from django.urls import path, include, re_path
 from rest_framework import permissions
 from django.conf import settings
@@ -35,6 +36,8 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/login/', LoginView.as_view(template_name='admin/login.html'), name='login'),
+    path('accounts/', include('django.contrib.auth.urls')),
     path('todos/', include('todos.urls')),
 ]
 
